1fbfe9c4ccfc5ae7b5a3b0b68433750732affc1c
Alan Chatham <alan.chatham@gmail.com>
2012-02-15 20:48:55 +1100

    First commit since moving from Hg.  Working. To do - New menu New icon/logo

---
+++ FPSNav_extension.rb
@@ -0,0 +1,40 @@
+=begin
+    FPS Navigator Plugin for Google SketchUp
+     Copyright 2011
+     Alan Chatham
+     
+    This file is part of FPS Navigator Plugin.
+
+    The FPS Navigator Plugin is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    The FPS Navigator Plugin is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with the FPS Navigator Plugin.  If not, see <http://www.gnu.org/licenses/>.
+
+=end
+
+# Loader for the FPSNav extension
+     require 'sketchup.rb'
+     require 'extensions.rb'
+
+     fpsNav = SketchupExtension.new "FPSNav", "FPSNav/FPSNav.rb"
+     fpsNav.version = '0.3'
+     fpsNav.copyright = "2012"
+     fpsNav.creator = "Alan Chatham <alan.chatham@gmail.com>"
+     fpsNav.description = "First-Person Shooter Style Navigation."
+     fpsNav.description += "  When active, move with the arrow keys; alternately, hold 'Shift' and move with WASD, or use the numeric keypad."
+     fpsNav.description += "  Movement is restricted to the X-Y Plane."
+     fpsNav.description += "  Pressing Shift + Q will move you down in the Z direction, and Shift + E will move you up."
+     fpsNav.description += "  Alternatively, 7 and 9 on the numeric keypad do the same"
+     fpsNav.description += "  Left-click the mouse and hold to look around."
+     fpsNav.description += "  Double-click to lock mouselook on until you click again."
+     fpsNav.description += "  Right-click and hold, and you will move 10 times as fast."
+     fpsNav.description += "  Movement speed and mouse sensitivity can be found in the 'Plugins/FPSNav' menu."
+     Sketchup.register_extension(fpsNav, true)
---
+++ HowToUse.txt
@@ -0,0 +1,6 @@
+First-Person Shooter Style Navigation
+
+When active, move with the arrow keys; alternately, hold 'Shift' and move with WASD.
+Movement is restricted to the X-Y Plane.  Pressing Shift + Q will move you down in the Z direction, and Shift + E will move you up.
+Left-click the mouse and hold to look around.
+Right-click and hold, and you will move 10 times as fast.
